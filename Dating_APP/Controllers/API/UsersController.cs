d57da349b9813020d46362780fdd7269a43136f5
gentritt <gentrit.selimi@riinvest.net>
2021-06-14 16:33:20 +0200

    Add project files.

---
+++ Dating_APP/Controllers/API/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using Dating_APP.Data;
+using Dating_APP.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Dating_APP.Controllers.API
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UsersController : ControllerBase
+	{
+		private readonly DataContext _context;
+
+		public UsersController(DataContext context)
+		{
+			_context = context;
+		}
+		[HttpGet]
+		public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+		{
+			var users = await _context.Users.ToListAsync();
+
+			return users;
+		}
+
+		//api/users/3
+		[HttpGet("{id}")]
+		public async Task<ActionResult<AppUser>> GetUsers(int id)
+		{
+			return await _context.Users.FindAsync(id);
+		}
+	}
+}
---
+++ Dating_APP/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Dating_APP.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dating_APP.Data
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions options) : base(options)
+		{
+
+		}
+		public DbSet<AppUser> Users { get; set; }
+	}
+}
