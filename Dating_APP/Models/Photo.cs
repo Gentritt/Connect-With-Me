ab1347569b3c19b318e7e7ee797576784d503af5
gentritt <gentrit.selimi@riinvest.net>
2021-07-05 15:23:14 +0200

    AppUser class Created

---
+++ Dating_APP/Models/Photo.cs
@@ -0,0 +1,11 @@
+﻿namespace Dating_APP.Models
+{
+	public class Photo
+	{
+		public int Id { get; set; }
+		public string ImageUrl { get; set; }
+		public bool IsMain { get; set; }
+		public string PublicId { get; set; }
+
+	}
+}
