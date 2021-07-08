a105c4db47d150efd8733d0bc391d1dd2dd48ba7
gentritt <gentrit.selimi@riinvest.net>
2021-07-08 14:17:26 +0200

    Member-Edit Profile implemeted on client-side and prevent unsaved changes

--- Dating_APP/client/src/app/app-routing.module.ts
+++ Dating_APP/client/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
 import { TestErrorsComponent } from './errors/test-errors/test-errors.component';
 import { AuthGuard } from './guards/auth.guard';
+import { PreventUnsavedChangesGuard } from './guards/prevent-unsaved-changes.guard';
 import { HomeComponent } from './home/home.component';
 import { ListsComponent } from './lists/lists.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
@@ -17,7 +18,7 @@ const routes: Routes = [
     { path: 'members/:username', component: MemberDetailComponent, canActivate: [AuthGuard] },
     { path: 'lists', component: ListsComponent, canActivate: [AuthGuard] },
   { path: 'messages', component: MessagesComponent, canActivate: [AuthGuard] },
-  { path: 'member/edit', component: MemberEditComponent, canActivate: [AuthGuard] },
+  { path: 'member/edit', component: MemberEditComponent, canDeactivate: [PreventUnsavedChangesGuard] },
   { path: 'errors', component: TestErrorsComponent },
   { path: 'not-found', component: NotFoundComponent },
   { path: 'server-error', component: ServerErrorComponent }, 
--- Dating_APP/client/src/app/members/member-edit/member-edit.component.ts
+++ Dating_APP/client/src/app/members/member-edit/member-edit.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
 import { take } from 'rxjs/operators';
 import { Member } from '../../models/member';
 import { User } from '../../models/user';
@@ -11,10 +13,17 @@ import { MembersService } from '../../services/members.service';
   styleUrls: ['./member-edit.component.css']
 })
 export class MemberEditComponent implements OnInit {
+  @ViewChild('editForm') editForm!: NgForm;
   member!: Member;
   user!: User;
+  @HostListener('window:beforeunload', ['$event']) unloadNotfication($event: any) {
+    if (this.editForm.dirty) {
+      $event.returnValue = true;
+    }
+  }
+    //acceess the browser events
 
-  constructor(private accountService: AccountService, private memberService: MembersService) {
+  constructor(private accountService: AccountService, private memberService: MembersService, private toastr: ToastrService) {
     this.accountService.currentUser$.pipe(take(1)).subscribe(user => this.user = user)
   }
 
@@ -26,5 +35,11 @@ export class MemberEditComponent implements OnInit {
     this.memberService.getMember(this.user.username).subscribe(member => {
       this.member = member;
     })
+  }
+
+  updateMember() {
+    console.log(this.member);
+    this.toastr.success('Profile updated succesfully');
+    this.editForm.reset(this.member);
   }
 }
